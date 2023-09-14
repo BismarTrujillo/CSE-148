215e1990d79eb5a85651bcb9ff6cbeb095cf3a9c
Bismar Trujillo <bismar20@hotmail.com>
2023-09-13 22:06:52 -0400

    part 3 completed

--- src/Ring.java
+++ src/Ring.java
@@ -1,48 +1,82 @@
-import java.time.Clock;
-
 public class Ring {
     double outerRadius;
     double innerRadius;
+    Ring largest;
+    Ring smallest;
 
-    public Ring(){
+
+    public Ring() {
         outerRadius = 2.0;
         innerRadius = 1.0;
     }
-    public Ring(double outer, double inner){
 
-        if (outer > inner){
+    public Ring(double outer, double inner) {
+        if (outer > inner) {
             outerRadius = outer;
             innerRadius = inner;
-        }else {
+        } else {
             System.out.println("Outer radius smaller or equals than inner radius");
         }
     }
+
     public double getOuterRadius() {
         return outerRadius;
     }
-    public void setOuterRadius(double outerRadius) {
 
-        if (outerRadius > innerRadius){
+    public void setOuterRadius(double outerRadius) {
+        if (outerRadius > innerRadius) {
             this.outerRadius = outerRadius;
-        }else {
+        } else {
             System.out.println("Outer radius smaller or equals than inner radius");
         }
     }
+
     public double getInnerRadius() {
         return innerRadius;
     }
-    public void setInnerRadius(double innerRadius) {
 
-        if (outerRadius > innerRadius){
+    public void setInnerRadius(double innerRadius) {
+        if (outerRadius > innerRadius) {
             this.innerRadius = innerRadius;
-        }else {
+        } else {
             System.out.println("Inner radius greater or equals than outer radius");
         }
     }
-    public double getArea(){
-        return (outerRadius*outerRadius - innerRadius*innerRadius) * Math.PI;
+
+    public double getArea() {
+        return (outerRadius * outerRadius - innerRadius * innerRadius) * Math.PI;
     }
-    public String toString(){
+
+    public static double averageAreaOfRings(Ring[] ringsArray) {
+        double areaSum = 0.0;
+        for (Ring ring : ringsArray) {
+            areaSum += ring.getArea();
+        }
+        return areaSum / ringsArray.length;
+    }
+
+    public static Ring smallestLargestRing(Ring[] ringsArray){
+        Ring [] smallestLargest = new Ring[2];
+        Ring smallest = ringsArray[0];
+        Ring largest = ringsArray[0];
+
+        for (Ring ring: ringsArray){
+            if(smallest.getArea() > ring.getArea()){
+                smallest = ring;
+            }
+            if(largest.getArea() < ring.getArea()){
+                largest = ring;
+            }
+        }
+
+        Ring ring = new Ring();
+        ring.smallest = smallest;
+        ring.largest = largest;
+
+        return ring;
+    }
+
+    public String toString() {
         return "Outer radius: " + outerRadius + " Inner radius: " + innerRadius + " Area: " + getArea();
     }
 
--- src/Test.java
+++ src/Test.java
@@ -3,32 +3,38 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Random rand = new Random();
-        Ring [] ringArray = new Ring[5];
+        Ring[] ringArray = new Ring[5];
+        Ring[] newRingArray = new Ring[3];
 
 
-        ringArray[0] = new Ring();
-        ringArray[1] = new Ring();
-        ringArray[2] = new Ring();
-        ringArray[3] = new Ring();
-        ringArray[4] = new Ring();
+        ringArray[0] = new Ring(3.1,2.1);
+        ringArray[1] = new Ring(4.2,1.2);
+        ringArray[2] = new Ring(8.4,4.2);
+        ringArray[3] = new Ring(9.1,4.5);
+        ringArray[4] = new Ring(3.5,1.2);
 
-        List<Ring> ringList= new ArrayList<>(Arrays.asList(ringArray));
+        int index1 = rand.nextInt(ringArray.length);
+        int index2;
 
-        List<Ring> deletedRings;
-        deletedRings = Collections.shuffle(ringList);
+        do {
+            index2 = rand.nextInt(ringArray.length);
+        } while (index2 == index1);
 
-        ringList.remove(0);
-        ringList.remove(0);
+        int newRingArrayIndex = 0;
+        for (int i = 0; i < ringArray.length; i++) {
+            if (i != index1 && i != index2) {
+                newRingArray[newRingArrayIndex] = ringArray[i];
+                newRingArrayIndex++;
+            }
+        }
+        System.out.println("\nUpdated array:");
+        for (Ring value : newRingArray) {
+            System.out.println(value);
 
+        }
+        System.out.println("The average area of rings is: " + Ring.averageAreaOfRings(ringArray));
 
-       Ring [] newRingArray = ringList.toArray(ringArray);
-
-        System.out.println(newRingArray.length);
-
-
-
-
+        System.out.println("The largest ring has area: " + Ring.smallestLargestRing(ringArray).largest.getArea() + "the smallest ring has area: " + Ring.smallestLargestRing(ringArray).smallest.getArea());
     }
-
 
 }
